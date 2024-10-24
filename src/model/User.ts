b35928fb8fe2b390a9c21278411fc3262cd116cd
Sakpal Amit <amitsakpal744@gmail.com>
2024-10-24 20:03:51 +0530

    completed teh user schema model along with added isverified in user interface to validate user verifications and changed the message to "messages" and added the message interface as its type as its an array

--- src/model/User.ts
+++ src/model/User.ts
@@ -28,8 +28,9 @@ export interface User extends Document{
     password:string; 
     verifyCode:string; 
     verifyCodeExpiry:Date;
+    isVerified:boolean;
     isAcceptingMessage:boolean;
-    message:Message[]
+    messages:Message[]
 
 }
 
@@ -46,6 +47,27 @@ const UserSchema : Schema<User> = new Schema ({
         required:[true,"Email is required"],
         unique:true,
         match:[/.+\@.+\..+/,'please use a valid email address']
-    }
+    },
+    password:{
+        type:String, 
+        required:[true,"Password is required"]
+    },
+    verifyCode:{
+        type:String, 
+        required:[true,"VerifyCode is required"]
+    },
+    verifyCodeExpiry:{
+        type:Date, 
+        required:[true,"Verify code Expiry is required"]
+    },
+    isVerified:{
+        type:Boolean, 
+        default:false
+    },
+    isAcceptingMessage:{
+        type:Boolean,
+        default:true
+    },
+    messages:[MessageSchema]
 
 })
