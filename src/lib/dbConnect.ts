5790245fed36565911e9d69407fff9e43e5a69bf
Sakpal Amit <amitsakpal744@gmail.com>
2024-10-25 12:15:56 +0530

    added validation to check if db is  already connected

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -16,4 +16,10 @@ const connection:ConnectionObject  = {
 // its gonna contain and return a promise , also the void is different then whats used in c++ // 
 async function dbConnect():Promise<void>{
 
+    // if already connected then return //
+    if(connection.isConnected){
+        console.log(`Already connected to the database`); 
+        return
+    }
+
 }
