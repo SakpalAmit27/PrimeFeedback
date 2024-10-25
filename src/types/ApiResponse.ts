59cb58a315f2776505a032a88de6d09d90ff9b7e
Sakpal Amit <amitsakpal744@gmail.com>
2024-10-25 15:25:28 +0530

    completed the APi interface response we want to show

--- src/types/ApiResponse.ts
+++ src/types/ApiResponse.ts
@@ -1,2 +1,9 @@
 // this file will derive the type of response we want to show // 
+import { Message } from "@/model/User";
 
+export interface ApiResponse{
+    success:boolean;
+    message:string;
+    isAcceptingMessages?:boolean
+    messages?:Array<Message>
+}
