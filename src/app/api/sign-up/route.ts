45b38de4457f711b4f26cae9a36326fecab7d4f1
Sakpal Amit <amitsakpal744@gmail.com>
2024-10-25 17:23:05 +0530

    updated the json

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -2,3 +2,11 @@ import dbConnect from "@/lib/dbConnect";
 
 import UserModel from "@/model/User";
 
+import bcrypt from "bcryptjs"
+
+import { sendVerificationEmail } from "@/helpers/sendVerificationEmail";
+
+
+export async function POST(request : Request){
+    
+}
