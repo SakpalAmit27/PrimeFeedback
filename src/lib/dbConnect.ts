89e82874c837c7eaaf8d27cbb8566b701bbecb61
Sakpal Amit <amitsakpal744@gmail.com>
2024-10-25 12:11:45 +0530

    on the way to implement connection and completed object

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose'
+
+// creating an connection object // 
+
+
+type ConnectionObject = {
+    isConnected?:number
+
+}
+
+// added our ConnectionObject here to derive that its a type 
+const connection:ConnectionObject  = {
+
+}
