c1b55e7c5ac4a30e5d6d5f73aa9f206ae2eec4fc
Sakpal Amit <amitsakpal744@gmail.com>
2024-10-26 13:44:25 +0530

    added options and validation in option ts

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,50 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User"; // Import User interface
+
+export const authOptions: NextAuthOptions = {
+    providers: [
+        CredentialsProvider({
+            id: "credentials",
+            name: "Credentials",
+            credentials: {
+                email: {label: "email", type: "text"},
+                password: {label: "Password", type: "password"}
+            },
+            async authorize(credentials:any): Promise<any> {
+                await dbConnect()
+                try {
+                    const user = await UserModel.findOne({
+                        $or:[
+                            {email: credentials.identifier},
+                            {username: credentials.identifier}
+                        ]
+                    })
+                    if(!user){
+                        throw new Error('no user found with this email/username')
+                    }
+
+                    if(!user.isVerified){
+                        throw new Error("please verfiy your account before logging in")
+                    }
+
+                    const isPasswordCorrect = await bcrypt.compare(credentials.password, user.password)
+                    if(isPasswordCorrect){
+                        return user
+                    }
+                    else{
+                        throw new Error('invalid password')
+                    }
+
+                } catch (error: any) {
+                    throw new Error(error)
+                }
+            }
+        })
+    ]
+    
+}
+
