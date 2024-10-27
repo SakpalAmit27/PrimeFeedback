0304a5b53c1e8b63d6c82c974ee81a7505821e47
Sakpal Amit <amitsakpal744@gmail.com>
2024-10-27 12:02:45 +0530

    added session statergy of jwt for auth to handle and sign in , and secret key of nextauth from env as well

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -47,7 +47,11 @@ export const authOptions: NextAuthOptions = {
     ],
     pages:{
         signIn:'/sign-in' 
-    }
+    },
+    session:{
+        strategy:"jwt"
+    },
+    secret:process.env.NEXTAUTH_SECRET
     
 }
 
