dbfe86d4df6cf7fd1e09d97808ea0f76077eb62f
Sakpal Amit <amitsakpal744@gmail.com>
2024-10-28 08:08:25 +0530

    added the types for _ and along with some other ones

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,2 +1,13 @@
 // for fixing the user._id error . gonna define the types here // 
 
+import "next-auth"
+
+declare module "next-auth"{
+    interface User{
+        _id?:string
+        isVerified:string
+        isAcceptingMessages?:boolean
+        username?:string
+
+    }
+}
